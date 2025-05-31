0ed1ff96ae8055424756893981fcead867aa9465
GanX <chenzhenggang1@gmail.com>
2025-05-31 11:18:17 +0800

    数据库迁移文件

---
+++ GanPersonWeb/Migrations/20250527002241_modify_0.01.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GanPersonWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class modify_001 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "area",
+                table: "SiteVisits",
+                newName: "Area");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastLoginAt",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProfileImageUrl",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TodayCommentCount",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PublishDate",
+                table: "Projects",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Filename",
+                table: "Images",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LastLoginAt",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ProfileImageUrl",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "TodayCommentCount",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Filename",
+                table: "Images");
+
+            migrationBuilder.RenameColumn(
+                name: "Area",
+                table: "SiteVisits",
+                newName: "area");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PublishDate",
+                table: "Projects",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
